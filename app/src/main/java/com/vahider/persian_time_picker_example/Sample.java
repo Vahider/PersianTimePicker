eaa6e562b3674cac04438a0979434a2bde0b6fe4
Vahid <hvm32@yahoo.com>
2020-02-25 16:31:16 +0330

    Create project

---
+++ app/src/main/java/com/vahider/persian_time_picker_example/Sample.java
@@ -0,0 +1,14 @@
+package com.vahider.persian_time_picker_example;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Sample extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
